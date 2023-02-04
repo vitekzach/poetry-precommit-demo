aa2ffc4b346a7944461c2c491b01efa5daa652f1
vitek <vitekzach@users.noreply.github.com>
2023-02-04 19:04:23 +0100

    add more hooks

---
+++ project-name/main.py
@@ -0,0 +1,27 @@
+﻿"""Module docstring."""
+
+print("Hello world")
+
+
+def fnc(i: int):
+    """
+    Comment.
+
+    Params:
+    a: int
+        some int
+    """
+    return f"yo {i}"
+
+
+def func2(j: str):
+    """
+    Definitely not a numpy style docstring.
+
+    Two lines
+    Params:
+    j
+
+    """
+    j = "a"
+    print(j)
